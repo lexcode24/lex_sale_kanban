ade52a7e5e41b0595613a7af0f5d3077dcd5a3d9
Alexander Kröker <lexcode@MacBook-Pro.fritz.box>
2022-01-31 13:10:33 +0100

    add stage model

--- lex_sale_kanban/models/models.py
+++ lex_sale_kanban/models/models.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+
+class LexSaleStage(models.Model):
+    _name = "lex.sale.stage"
+
+    name = fields.Char(string="Name")
+    is_default = fields.Boolean(string="Default Stage")
