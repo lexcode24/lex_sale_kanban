7cb35e3fa73355aa087b039e29f72aaf4c1e8f5c
Alexander Kröker <lexcode@MacBook-Pro.fritz.box>
2022-01-31 13:29:48 +0100

    update lex_sale_stage

--- lex_sale_kanban/models/models.py
+++ lex_sale_kanban/models/models.py
@@ -2,8 +2,20 @@
 
 from odoo import models, fields, api
 
+
 class LexSaleStage(models.Model):
     _name = "lex.sale.stage"
 
     name = fields.Char(string="Name")
-    is_default = fields.Boolean(string="Default Stage")
+    is_default = fields.Boolean(string="Default Stage", default=False)
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _get_default_stage(self):
+        stages = self.env['lex.sale.stage'].search([('is_default', '=', True)])
+        if stages:
+            return stages[0].id
+
+    lex_stage_id = fields.Many2one('lex.sale.stage', string="Stage", default=_get_default_stage)
